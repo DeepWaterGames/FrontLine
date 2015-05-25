1f9a5efd3a96f5fa3528c25483eb99663038a70a
BattleDolphin15 <nickcheng15@gmail.com>
2015-05-24 21:31:05 -0700

    Big internal changes to the effect system. Splitting of flFxGL.h soon to come
    Signed-off-by: BattleDolphin15 <nickcheng15@gmail.com>

--- Source/Frontline Engine/Frontline Engine/Graphics/GL.h
+++
@@ -1,9 +0,0 @@
-#include <GL/glew.h>
-#include <SFML/Audio.hpp>
-#include <SFML/Config.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/OpenGL.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <glm/ext.hpp>
